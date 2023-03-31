9c3f7d41c89bc1754a28af1398db2fae9b144067
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-30 21:15:31 -0300

    feat: 01) aula 05
    criado componente titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Cabecalho from './components/cabecalho';
 import Container from './components/container';
+import Titulo from './components/titulo';
 
 
 const App = () => {
     return (
         <>
             <Cabecalho />
-            <Container />
+            <Container>
+                <Titulo>Área Administrativa</Titulo>
+            </Container>
         </>
     );
 };
